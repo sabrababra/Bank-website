24fb089b5b620f2ad62f6fc0297bcb2a829b3c5b
sabrababra <sabrinayesminprome@gmail.com>
2022-08-16 20:08:58 +0600

    bank page done

--- js/login.js
+++ js/login.js
@@ -1,1 +1,14 @@
-console.log('hi');
+document.getElementById('btn-submit').addEventListener('click',function(){
+
+    const userEmail= document.getElementById('user-email').value;
+    const userPass= document.getElementById('user-pass').value;
+    if(userEmail=="prome@gmail.com" && userPass==="prome123"){
+        location.href = "bank.html";
+    }else{
+        alert ('Please enter valid info');
+        // console.log(userEmail);
+        // console.log(userPass);
+        
+    }
+   
+});
